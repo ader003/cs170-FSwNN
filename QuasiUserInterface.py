bd17ab1482a7120a358a2f6da4ef93f0c02434ae
Audrey Der <ader003@ucr.edu>
2017-11-28 11:18:52 -0800

    np dot loadtxt and first set of hints' code

--- QuasiUserInterface.py
+++ QuasiUserInterface.py
@@ -1,5 +1,6 @@
 import numpy as np
-import NN.py
+import NN
+import random
 import heapq as queue
 
 
@@ -13,22 +14,7 @@ def select_algorithm():
 
     # print("Please hold while the data is normalized.")
     # normalize_data()
-
-    if algorithm == "1":
-        forward_selection()
-
-    if algorithm == "2":
-        backwards_elimination()
-
-    if algorithm == "3":
-        propinqua()
-
-
-def create_feature_sets():  # pass in the number of features, or detect the number of features?
-    # TODO: How to manage the overloaded operator?
-    # TODO: CREATE TREE OF FEATURE SETS TO EXPLORE
-
-    return queue
+    return algorithm
 
 
 def normalize_data():
@@ -50,6 +36,47 @@ def propinqua():
     return
 
 
-def __main__():
+def read_in_data(filename):
+    return np.loadtxt(filename)
+
+
+def feature_search(data, search_algorithm):
+    current_set_of_features = []
+    for i in size(data, 2) - 1:
+        print("On the ", i, "th ", "level of the search tree")
+        feature_to_add_at_this_level = []
+        current_best_accuracy = 0
+        for k in size(data, 2) - 1:
+            if k in current_set_of_features:
+                pass
+            else:
+                print("--Considering adding the ", i, " feature")
+                accuracy = random.random() # FUNCTION STUB; TODO: FIX
+                # if algorithm == 1:
+                #     forward_selection()
+                # if algorithm == 2:
+                #     backwards_elimination()
+                # if algorithm == 3:
+                #    propinqua()
+
+            if accuracy > current_best_accuracy:
+                current_best_accuracy = accuracy * 100
+                feature_to_add_at_this_level = k
+        current_set_of_features[i] = feature_to_add_at_this_level
+        print("On level ", i, " I added feature ", feature_to_add_at_this_level, " to the current set of features used.")
+
+
+def main():
+    filename = input("What file's data will be used?")
+    algorithm = select_algorithm()
+    if algorithm == "1":
+        feature_search(read_in_data(filename, 1))
+    if algorithm == "2":
+       feature_search(read_in_data(filename, 2))
+    if algorithm == "3":
+       feature_search(read_in_data(filename, 3))
 
     return
+
+if __name__ == '__main__':
+    main()
--- nn.py
+++ nn.py
@@ -1,26 +1,13 @@
 import heapq as point
 import math
-# import numpy as np
+import numpy as np
 
 
 class DataPoint:
 
-        def __init__(self, one, two, three, four, five, six, seven, eight, nine, ten, eleven, filename, feature_set):
-            # setting all the features' values
-            self.one = one  # the classification: a one or a two
-            self.two = two
-            self.three = three
-            self.four = four
-            self.five = five
-            self.six = six
-            self.seven = seven
-            self.eight = eight
-            self.nine = nine
-            self.ten = ten
-            self.eleven = eleven
+        def __init__(self, feature_set):
             self.feature_set = feature_set
             create_float_line_data(filename)
-
 
 def create_float_line_data(filename):
     line_data = []
@@ -32,11 +19,29 @@ def create_float_line_data(filename):
     return line_data
 
 
-def find_smallest_distance(feature_set, node_a, node_b):
+def find_smallest_distance(): #PUT IN DISTANCE FORMULA FOR MULTIPLE FEATURES + ACTUAL MATH
     smallest_distance = 0
-    for feature in feature_set:  # TODO: is there a way to tag "one" with 1 (the int)?
-        distance = math.sqrt(pow((node_a.one - node_b.one), 2.0) + pow((node_a.two - node_b.two), 2.0))
-        if distance < smallest_distance:
-            smallest_distance = distance  # TODO: Determine if deep copy is needed for distance and smallest_distance
+    return smallest_distance, most_accurate_feature #returns the smallest distance and most accurate feature
+
+
+# takes in the set of features
+def find_nearest_neighbor(feature_set): #TODO: PUT IN LEGITIMATE MATH
+    smallest_distance = 0
+    for i in feature_set:
+        first_feature = smallest_distance(feature_set)[1]
+        for j in feature_set:
+            if j is not first_feature:
+                distace_to_root += pow(j - i, 2.0)
+
+
+    smallest_distance = sqrt(distance_to_root)
     return smallest_distance
+
+
+def expand_children(current_list_of_unsused_features):
+    return
+
+
+def pop_feature(current_list_of_unused_features):
+    return
 
