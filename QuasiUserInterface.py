88c512eea2ed7b25dfe6a6fd1d959829549b74c1
Audrey Der <ader003@ucr.edu>
2017-11-28 17:34:52 -0800

    backwards elimination and forward selection

--- QuasiUserInterface.py
+++ QuasiUserInterface.py
@@ -1,7 +1,6 @@
 import numpy as np
 import NN
 import random
-import heapq as queue
 
 
 def select_algorithm():
@@ -21,60 +20,73 @@ def normalize_data():
     return
 
 
-def forward_selection():
+def forward_selection(data):
+    # add features one by one until the most accurate set of features is found
+    #print("FIRST LINE OF DATA: ", data[0], '\n') 
+    #print("Length of data[0]: ", len(data[0]), '\n') CONSENSUS: Data is read in correctly; len(data[i]) = 11
+    current_set_of_features = []
     print("Beginning search.")
+    for i in range(0, len(data)):
+        print("On the ", i, "th ", "level of the search tree")
+        feature_added_at_this_level = 0
+        current_best_accuracy = 0.0
+        for k in range(0, len(data[i])):
+            if k in current_set_of_features:
+                pass
+            else:
+                #print("--Considering adding the ", k, " feature")
+                accuracy = random.random() # FUNCTION STUB; TODO: FIX
+
+            if accuracy > current_best_accuracy:
+                current_best_accuracy = accuracy * 100
+                feature_addded_at_this_level = k
+        current_set_of_features.append(feature_added_at_this_level)
+        print("On level ", i, " I added feature ", feature_added_at_this_level, " to the current set of features used.")
+    print("Using features ", current_set_of_features, " accuracy is ", current_best_accuracy, "%")
     return
 
 
-def backwards_elimination():
-    print("Beginning search.")
+def backwards_elimination(data):
+    list_of_removed_features = []
+    # begin with all features and delete them one by one, such that same minimum is found
+    for i in data[0] in range(1, len(data[0])):
+        current_set_of_features.append(i) #place all features in a list
+    current_best_accuracy = find_accuracy(data)
+    for k in range(0, len(data[0])):
+        if k in list_of_removed_features:
+            pass
+        else:
+            if accuracy_without_feature(current_set_of_features, h) > current_best_accuracy:
+                #if the accuracy without the feature is greater than with it, append the feature to the list detailing the removed features, and remove it from the set of features to check
+                list_of_removed_features.append(h)
+                current_set_of_features.remove(h)
+    print("Using features ", current_set_of_features, " accuracy is ", current_best_accuracy, "%")
     return
-
 
 def propinqua():
-    print("Beginning search.")
+
     return
 
 
 def read_in_data(filename):
     return np.loadtxt(filename)
 
+# https://docs.scipy.org/doc/numpy-1.13.0/reference/index.html
+def find_accuracy(data):
+    return
 
-def feature_search(data, search_algorithm):
-    current_set_of_features = []
-    for i in size(data, 2) - 1:
-        print("On the ", i, "th ", "level of the search tree")
-        feature_to_add_at_this_level = []
-        current_best_accuracy = 0
-        for k in size(data, 2) - 1:
-            if k in current_set_of_features:
-                pass
-            else:
-                print("--Considering adding the ", i, " feature")
-                accuracy = random.random() # FUNCTION STUB; TODO: FIX
-                # if algorithm == 1:
-                #     forward_selection()
-                # if algorithm == 2:
-                #     backwards_elimination()
-                # if algorithm == 3:
-                #    propinqua()
-
-            if accuracy > current_best_accuracy:
-                current_best_accuracy = accuracy * 100
-                feature_to_add_at_this_level = k
-        current_set_of_features[i] = feature_to_add_at_this_level
-        print("On level ", i, " I added feature ", feature_to_add_at_this_level, " to the current set of features used.")
-
+def accuracy_without_feature(set_of_features, remove_feature):
+    return
 
 def main():
-    filename = input("What file's data will be used?")
+    filename = input("What file's data will be used? ")
     algorithm = select_algorithm()
     if algorithm == "1":
-        feature_search(read_in_data(filename, 1))
+        feature_search(read_in_data(filename), 1)
     if algorithm == "2":
-       feature_search(read_in_data(filename, 2))
+       feature_search(read_in_data(filename), 2)
     if algorithm == "3":
-       feature_search(read_in_data(filename, 3))
+       feature_search(read_in_data(filename), 3)
 
     return
 
