3c44ec4b873b9c9ef3dfd73beab198e18e83bf5d
Audrey Der <ader003@ucr.edu>
2017-11-28 18:01:09 -0800

    correction: added high level algorithms for forward selection and backwards elimination; also skeleton for nearest neighbor

--- QuasiUserInterface.py
+++ QuasiUserInterface.py
@@ -1,6 +1,7 @@
 import numpy as np
 import NN
 import random
+import copy
 
 
 def select_algorithm():
@@ -36,6 +37,7 @@ def forward_selection(data):
             else:
                 #print("--Considering adding the ", k, " feature")
                 accuracy = random.random() # FUNCTION STUB; TODO: FIX
+                #accuracy = find_accuracy(current_set_of_features)
 
             if accuracy > current_best_accuracy:
                 current_best_accuracy = accuracy * 100
@@ -51,15 +53,19 @@ def backwards_elimination(data):
     # begin with all features and delete them one by one, such that same minimum is found
     for i in data[0] in range(1, len(data[0])):
         current_set_of_features.append(i) #place all features in a list
+        testing_set_of_features = copy.current_set_of_features
     current_best_accuracy = find_accuracy(data)
     for k in range(0, len(data[0])):
         if k in list_of_removed_features:
             pass
         else:
-            if accuracy_without_feature(current_set_of_features, h) > current_best_accuracy:
+            testing_set_of_features.remove(h)
+            if find_accuracy(data, testing_set_of_features) > current_best_accuracy:
                 #if the accuracy without the feature is greater than with it, append the feature to the list detailing the removed features, and remove it from the set of features to check
                 list_of_removed_features.append(h)
                 current_set_of_features.remove(h)
+            else:
+                testing_set_of_features = copy.current_set_of_features() #TODO: MAKE MORE EFFICIENT
     print("Using features ", current_set_of_features, " accuracy is ", current_best_accuracy, "%")
     return
 
@@ -72,14 +78,17 @@ def read_in_data(filename):
     return np.loadtxt(filename)
 
 # https://docs.scipy.org/doc/numpy-1.13.0/reference/index.html
-def find_accuracy(data):
-    return
-
-def accuracy_without_feature(set_of_features, remove_feature):
-    return
+def find_accuracy(set_of_features, data): #TODO: Nearest neighbor
+    classifications_correct = 0
+    for i in data:
+        # TODO : MATH GOES HERE
+        result = 0 # THE RESULT GUESSED BY THE ALGORITHM
+        if result == data[i]:
+            classifications_correct = classifications_correct + 1
+    return classifications_correct / len(data)
 
 def main():
-    filename = input("What file's data will be used? ")
+    filename = input("Type in the name of the file to test: ")
     algorithm = select_algorithm()
     if algorithm == "1":
         feature_search(read_in_data(filename), 1)
