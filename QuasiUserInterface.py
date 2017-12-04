1bb2c916da1cbdc636c8802bfa99885fb6fc34ec
Audrey Der <ader003@ucr.edu>
2017-12-04 01:07:20 -0800

    Backwards elimination draft

--- QuasiUserInterface.py
+++ QuasiUserInterface.py
@@ -25,16 +25,16 @@ def normalize_data(data):
 
 def forward_selection(data):
     curr_set_of_features = []
-    start_time = time.time()
     global_best_acc = 0.0 # global best accuracy
-    overall_best_feat_set = [] # global best feature 
+    overall_best_feat_set = [] # global best feature
+    start_time = time.time()
     for i in range(1, len(data[0]) - 1): # acts as a multiplier; how many times to run the inner loop
         feat_to_add = 0
         local_best_acc = 0.0 # best recorded accuracy for local levels
         for k in range(1, len(data[0] - 1)): # runs through the features and calculates accuracy based on the current set with the new addition
             if k not in curr_set_of_features:
                 # acc = random.random()
-                acc = find_accuracy(curr_set_of_features, data, k)
+                acc = find_accuracy(curr_set_of_features, data, k, 1)
                 if acc > local_best_acc:
                     local_best_acc = acc
                     feat_to_add = k
@@ -46,27 +46,32 @@ def forward_selection(data):
         print("Accuracy at current level: ", local_best_acc)
     end_time = time.time()
     print("Set of features used: ", overall_best_feat_set, "At accuracy: ", global_best_acc, '\n', "Elapsed time: ", end_time - start_time)
+    return
 
 
 def backwards_elimination(data):
-    list_of_removed_features = []
-    # begin with all features and delete them one by one, such that same minimum is found
-    for i in data[0] in range(1, len(data[0])):
-        current_set_of_features.append(i) #place all features in a list
-        testing_set_of_features = copy.current_set_of_features
-    current_best_accuracy = find_accuracy(data)
-    for k in range(0, len(data[0])):
-        if k in list_of_removed_features:
-            pass
-        else:
-            testing_set_of_features.remove(h)
-            if find_accuracy(testing_set_of_features, data) > current_best_accuracy:
-                #if the accuracy without the feature is greater than with it, append the feature to the list detailing the removed features, and remove it from the set of features to check
-                list_of_removed_features.append(h)
-                current_set_of_features.remove(h)
-            else:
-                testing_set_of_features = copy.current_set_of_features() #TODO: MAKE MORE EFFICIENT
-    print("Using features ", current_set_of_features, " accuracy is ", current_best_accuracy, "%")
+    global_best_acc = 0.0 # global best accuracy
+    overall_best_feat_set = []
+    curr_set_of_features = [i for i in range(1, len(data))]
+    start_time = time.time()
+    for i in range(1, len(data[0] - 1)):
+        feat_to_pop = 0
+        local_best_acc = 0.0 # best recorded accuracy for local levels
+        for j in range(1, len(data[0]) - 1):
+            for k in range(1, len(data[0]) - 1):
+                if k in curr_set_of_features:
+                    #acc = find_accuracy(curr_set_of_features, data, k, 2)
+                    acc = random.random()
+                    if acc > local_best_acc:
+                        local_best_acc = acc
+                        feat_to_pop = k
+            if feat_to_pop in curr_set_of_features: #TODO: CHECK W JOC
+                curr_set_of_features.remove(feat_to_pop) # removes feature selected by inner for loop
+            if local_best_acc > global_best_acc: # check for decrease in accuracy
+                global_best_acc = local_best_acc
+                overall_best_feat_set = list(curr_set_of_features)
+    end_time = time.time()
+    print("Set of features used: ", overall_best_feat_set, "At accuracy: ", global_best_acc, '\n', "Elapsed time: ", end_time - start_time)
     return
 
 def propinqua():
@@ -79,16 +84,19 @@ def read_in_data(filename):
 
 
 # https://docs.scipy.org/doc/numpy-1.13.0/reference/index.html
-def find_accuracy(set_of_features, data, test_feature):
+def find_accuracy(set_of_features, data, test_feature, algorithm):
     test_feat_set = list(set_of_features)
-    test_feat_set.append(test_feature)
+    if algorithm == 1: #forward selection
+        test_feat_set.append(test_feature)
+    if algorithm == 2: #backwards elimination
+        test_feat_set.remove(test_feature)
     num_correct_classifications = 0
     local_shortest_distance = math.inf
     result = 0 # will be either 1 or 2
     for i in data:
         local_shortest_distance = math.inf
         for h in data:
-            if not np.array_equal(h, i): #checks if h and i are not the same row (aka, data point)
+            if not np.array_equal(h, i): #checks if h and i are not the same row
                 distance = 0
                 for j in test_feat_set:
                     distance += pow((i[j] - h[j]), 2.0) # n-space Euclidean distance formula
